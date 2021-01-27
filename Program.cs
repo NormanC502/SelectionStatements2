04611c4d82228da17640582114a8f62207143fd3
Norman Calhoun <shakauluz73@yahoo.com>
2021-01-27 15:24:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SelectionStatements2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("What is your subject choice?");
+            string userSubjectChosen = Console.ReadLine();
+
+            switch (userSubjectChosen)
+            {
+                case "English":
+                    Console.WriteLine("You have chosen wisely!");
+                    break;
+                case "Math":
+                    Console.WriteLine("Enjoy Algebra 1!");
+                    break;
+                case "Art":
+                    Console.WriteLine("You are today's Michaelangelo!");
+                    break;
+                case "Composition":
+                    Console.WriteLine("You are Langston Hughes!");
+                    break;
+                case "Humanities 1":
+                    Console.WriteLine("You are a history buff");
+                    break;
+            } 
+
+        }
+    }
+}
